29dc60173bf12e0d6c994a9239bd016a5e3c480a
Taylor Dorsett <taylorsunwantedemails@gmail.com>
2020-09-08 15:14:23 -0500

    Create functions_practice_solutions.js
    Add solution 1

---
+++ solution_key/functions_practice_solutions.js
@@ -0,0 +1,28 @@
+// 1. Write a function that takes in a number and returns the number times two. Then run the function and print the result.
+
+let timesTwo = function (n1) {
+  return n1 * n1
+}
+
+let multiply = timesTwo(1009)
+console.log(multiply)
+
+
+//   2. Write a function that takes in a string and returns the string with all capital letters.Then run the function and print the result.
+
+//   3. Write a function that takes in two numbers and returns the first number subtracted by the second.Then run the function and print the result.
+
+//   4. Write a function that takes in a number and returns the number times itself.Then run the function and print the result.
+
+//   5. Write a function that takes in a string and returns the first letter of the string.Then run the function and print the result.
+
+//   6. Write a function that takes in three strings and returns a string that combines all three strings with spaces in between.Then run the function and print the result.
+
+//   7. Write a function that takes in a number and returns the number as a string.Then run the function and print the result.
+
+//   8. Write a function that takes in a string and returns the string repeated 5 times.Then run the function and print the result.
+
+//   9. Write a function that takes in 3 numbers and returns the average(the sum divided by 3.0).Then run the function and print the result.
+
+//   10. Write a function that takes in a number and returns the number times 10 plus 30. Then run the function and print the result.
+
