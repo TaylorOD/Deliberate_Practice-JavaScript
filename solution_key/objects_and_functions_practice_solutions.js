cfcd5a48288d063251f9106e297e0f149eb20954
Taylor Dorsett <taylorsunwantedemails@gmail.com>
2020-09-09 14:49:30 -0500

    Update objects_and_functions_practice_solutions.js
    add 3rd practice problem.

--- solution_key/objects_and_functions_practice_solutions.js
+++ solution_key/objects_and_functions_practice_solutions.js
@@ -10,7 +10,7 @@ console.log(personInfo.firstName)
 console.log(personInfo.lastName)
 console.log(personInfo.emailAddress)
 
-// # 2. Make a object to store prices for 3 different menu items.Then print the price of the third to see the result.
+// # 2. Make a object to store prices for 3 different menu items. Then print the price of the third to see the result.
 
 let menuItems = {
   burger: 5.50,
@@ -22,7 +22,17 @@ console.log(menuItems.drink)
 
 // # 3. Make a object to store a book's title, author, number of pages, and language. Then print each attribute on separate lines.
 
+let bookStoreInfo = {
+  title: "The Shining",
+  author: "Stephen King",
+  numOfPages: 1056,
+  language: "English"
+}
 
+console.log(bookStoreInfo.title)
+console.log(bookStoreInfo.author)
+console.log(bookStoreInfo.numOfPages)
+console.log(bookStoreInfo.language)
 
 // # 4. Make objects to store the title and author for 3 different books.Then print out the third book's author using a function.
 
