0433b2f5a2ab49dc49af6f4b0ef06532f0ae2f9e
Taylor Dorsett <taylorsunwantedemails@gmail.com>
2020-09-26 16:57:11 -0500

    Update loops_practice_solutions.js
    - add 7th solution

--- solution_key/loops_practice_solutions.js
+++ solution_key/loops_practice_solutions.js
@@ -62,6 +62,13 @@ while (true) {
 
 // # 7. Write a while loop that prints the numbers 50 to 70.
 
+let index = 50
+
+while (index <= 70) {
+  console.log(index)
+  index += 1
+}
+
 // # 8. Write a while loop that prints the phrase "Around the world" 144 times.
 
 // # 9. Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
