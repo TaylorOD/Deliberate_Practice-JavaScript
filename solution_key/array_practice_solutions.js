9f3cdc16ed7d4804e40e19002b020d5c551f9452
Taylor Dorsett <taylorsunwantedemails@gmail.com>
2020-09-26 21:36:54 -0500

    Update array_practice_solutions.js
    - add 8th solution

--- solution_key/array_practice_solutions.js
+++ solution_key/array_practice_solutions.js
@@ -51,6 +51,10 @@ console.log(strings
 
 // # 8. Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
 
+let numbers = [5, 10, 22, 29, 100]
+numbers[0] = numbers[0] * 10
+console.log(numbers)
+            
 // # 9. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
 
 // # 10. Create an array to store names of 3 different countries. Then add one more country and print the array one line.
