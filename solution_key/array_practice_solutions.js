14ecb2c9ef36446372aa38411ab3d227cd3e76de
Taylor Dorsett <taylorsunwantedemails@gmail.com>
2020-09-26 21:35:08 -0500

    Update array_practice_solutions.js
    - add 7th solution

--- solution_key/array_practice_solutions.js
+++ solution_key/array_practice_solutions.js
@@ -45,6 +45,10 @@ console.log(names[2])
 
 // # 7. Create an array to store 2 strings. Then add one string to the array and print the array on one line.
 
+let strings = ["word", "leg kick"]
+strings.unshift("knee strike")
+console.log(strings
+
 // # 8. Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
 
 // # 9. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
