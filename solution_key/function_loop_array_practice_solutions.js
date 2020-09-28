37a6eb8e9e2f00c5bd167af3e1cb51d68746bfbd
Taylor Dorsett <taylorsunwantedemails@gmail.com>
2020-09-28 17:18:13 -0500

    Update function_loop_array_practice_solutions.js
    - add 3rd solution

--- solution_key/function_loop_array_practice_solutions.js
+++ solution_key/function_loop_array_practice_solutions.js
@@ -21,6 +21,11 @@ while (true) {
 
 // # 3. Write a function that takes in a string and returns the first letter of the string. Then run the function and print the result.
 
+let firstOfString = function (string) {
+  return string[0]
+}
+console.log(firstOfString("knight"))
+
 // # 4. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
 
 // # 5. Write a while loop that prints the phrase "Around the world" 144 times.
