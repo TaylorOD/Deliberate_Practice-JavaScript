690856eb4a9fe09f403399798c65a8c2d99cca32
Taylor Dorsett <taylorsunwantedemails@gmail.com>
2020-09-29 16:57:41 -0500

    Update functions_practice_solutions.js
    - refactor 8th solution

--- solution_key/functions_practice_solutions.js
+++ solution_key/functions_practice_solutions.js
@@ -13,51 +13,49 @@ const capitalStrings = function (string) {
 }
 console.log(capitalStrings("hello there!"))
 
-//   3. Write a function that takes in two numbers and returns the first number subtracted by the second.Then run the function and print the result.
+//   3. Write a function that takes in two numbers and returns the first number subtracted by the second. Then run the function and print the result.
 
 const firstMinusSecond = function (number1, number2) {
   return number1 - number2
 }
 console.log(firstMinusSecond(1000, 100))
 
-//   4. Write a function that takes in a number and returns the number times itself.Then run the function and print the result.
+//   4. Write a function that takes in a number and returns the number times itself. Then run the function and print the result.
 
 const numberTimesNumber = function (number) {
   return number * number
 }
 console.log(numberTimesNumber(19))
 
-//   5. Write a function that takes in a string and returns the first letter of the string.Then run the function and print the result.
+//   5. Write a function that takes in a string and returns the first letter of the string. Then run the function and print the result.
 
 const firstOfString = function (string) {
   return string[0]
 }
 console.log(firstOfString("taste"))
 
-//   6. Write a function that takes in three strings and returns a string that combines all three strings with spaces in between.Then run the function and print the result.
+//   6. Write a function that takes in three strings and returns a string that combines all three strings with spaces in between. Then run the function and print the result.
 
 const combineThreeStrings = function (string1, string2, string3) {
   return string1 + " " + string2 + " " + string3
 }
 console.log(combineThreeStrings("water", "taste", "good"))
 
-//   7. Write a function that takes in a number and returns the number as a string.Then run the function and print the result.
+//   7. Write a function that takes in a number and returns the number as a string. Then run the function and print the result.
 
 const numberToString = function (number) {
   return number.toString()
 }
 console.log(numberToString(199))
 
-//   8. Write a function that takes in a string and returns the string repeated 5 times.Then run the function and print the result.
+//   8. Write a function that takes in a string and returns the string repeated 5 times. Then run the function and print the result.
 
-let stringTimesFive = function (string) {
+const stringFiveTimes = function (string) {
   return string.repeat(5)
 }
-let stopYell = stringTimesFive("STOP")
-console.log(stopYell)
+console.log(stringFiveTimes("loops"))
 
-
-//   9. Write a function that takes in 3 numbers and returns the average(the sum divided by 3.0).Then run the function and print the result.
+//   9. Write a function that takes in 3 numbers and returns the average(the sum divided by 3.0). Then run the function and print the result.
 
 let average = function (n1, n2, n3) {
   return (n1 + n2 + n3) / 3
