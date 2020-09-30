7fcb251d2327dc7dc1796fecfbf5d26cce3f474d
Taylor Dorsett <taylorsunwantedemails@gmail.com>
2020-09-29 23:13:17 -0500

    Update each_practice_solutions.js
    - add first solution

--- solution_key/each_practice_solutions.js
+++ solution_key/each_practice_solutions.js
@@ -1,6 +1,12 @@
 // #  1. Start with an array of numbers and create a new array with each number times 3.
 // #     For example, [1, 2, 3] becomes [3, 6, 9].
 
+let numbers = [1, 2, 3]
+
+numbers.forEach(function (number) {
+  console.log(number * 3)
+})
+
 // #  2. Start with an array of strings and create a new array with each string upcased.
 // #     For example, ["hello", "goodbye"] becomes ["HELLO", "GOODBYE"].
 
