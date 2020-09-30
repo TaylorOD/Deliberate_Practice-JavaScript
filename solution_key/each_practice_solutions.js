8b124f808da6ee7e8132444ed952db88cbfb876b
Taylor Dorsett <taylorsunwantedemails@gmail.com>
2020-09-30 14:27:15 -0500

    Update each_practice_solutions.js
    - add 5th solution

--- solution_key/each_practice_solutions.js
+++ solution_key/each_practice_solutions.js
@@ -47,6 +47,13 @@ numbers.forEach(function (number) {
 // #  5. Start with an array of strings and create a new array with each string's length.
 // #     For example, ["hello", "goodbye"] becomes [5, 7].
 
+let strings = ["hello", "goodbye"]
+strings.forEach(function (string) {
+  let stringsLength = []
+  stringsLength.push(string.length)
+  console.log(stringsLength)
+})
+
 // #  6. Start with an array of hashes and create a new array of number values from each hash's :age key.
 // #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [27, 16].
 
