cb2e970cbabfbb565d987f07691df87de1e35c5c
Taylor Dorsett <taylorsunwantedemails@gmail.com>
2020-10-03 18:40:36 -0500

    Update hash-object_practice_solutions.js
    - add 5th solution

--- solution_key/hash-object_practice_solutions.js
+++ solution_key/hash-object_practice_solutions.js
@@ -54,6 +54,24 @@ console.log(bookInfo.language)
 
 // # 5. Make an array of hashes to store the title and author for 3 different books. Then print out the third book's author.
 
+let booksInfo = [{
+
+  title: "The Catcher in the Rye",
+  author: "J. D. Salinger",
+
+}, {
+
+  title: "Franny and Zooey",
+  author: "J. D. Salinger",
+
+}, {
+
+  title: "Nine Stories",
+  author: "J. D. Salinger",
+
+}]
+console.log(booksInfo[2].author)
+
 // # 6. Make a hash to store 3 different states and their captitals. Then add a new state and capital and print the hash to see the result.
 
 // # 7. Make a hash to store a laptop's brand, model, and year. Then print each attribute on separate lines.
