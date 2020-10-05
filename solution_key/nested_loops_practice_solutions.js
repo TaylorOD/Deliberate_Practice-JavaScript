383c0a77d1c77d13301440a5e42fe15d12bb9da7
Taylor Dorsett <taylorsunwantedemails@gmail.com>
2020-10-04 21:57:45 -0500

    Update nested_loops_practice_solutions.js
    - add 7th solution

--- solution_key/nested_loops_practice_solutions.js
+++ solution_key/nested_loops_practice_solutions.js
@@ -116,6 +116,19 @@ console.log(result)
 // #  7. Use a nested loop with an array of numbers to compute an array with every combination of products from each number.
 // #     For example, [2, 8, 3] becomes [4, 16, 6, 16, 64, 24, 6, 24, 9].
 
+let numbers = [2, 8, 3]
+let result = []
+let index1 = 0
+while (index1 < numbers.length) {
+  let index2 = 0
+  while (index2 < numbers.length) {
+    result.push(numbers[index1] * numbers[index2])
+    index2 += 1
+  }
+  index1 += 1
+}
+console.log(result)
+
 // #  8. Use a nested loop to find the largest sum of any two different numbers within an array.
 // #     For example, [1, 8, 3, 10] becomes 18.
 
