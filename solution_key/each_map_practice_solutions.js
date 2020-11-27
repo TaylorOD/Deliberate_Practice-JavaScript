c23c7222726262ecf3d43caff3fbd2550f8317b9
Taylor Dorsett <taylorsunwantedemails@gmail.com>
2020-11-26 20:44:22 -0600

    Update each_map_practice_solutions.js
    - add 2nd and 3rd solution

--- solution_key/each_map_practice_solutions.js
+++ solution_key/each_map_practice_solutions.js
@@ -1,7 +1,7 @@
 // #  1. Start with an array of numbers and create a new array with each number times 3.
 // #     For example, [1, 2, 3] becomes [3, 6, 9]. Use .forEach and .map.
 
-// ..forEach
+// .forEach
 
 let numbers = [1, 2, 3]
 let numbersTimesThree = []
@@ -29,23 +29,62 @@ console.log(numbersTimesThreeMapLong)
 // #  2. Start with an array of strings and create a new array with each string upcased.
 // #     For example, ["hello", "goodbye"] becomes ["HELLO", "GOODBYE"]. Use .forEach and .map.
 
-// ..forEach
+// .forEach
+let strings = ["hello", "goodbye"]
+let stringsUpcasedForEach = []
 
+strings.forEach(string => stringsUpcasedForEach.push(string.toUpperCase()))
+console.log(stringsUpcasedForEach)
+
+let stringsUpcasedForEachLong = []
+strings.forEach(function(string) {
+  stringsUpcasedForEachLong.push(string.toUpperCase())
+})
+console.log(stringsUpcasedForEachLong)
 
 // .map
+
+const stringsUpcasedMap = strings.map(string => string.toUpperCase())
+console.log(stringsUpcasedMap)
+
+const stringsUpcasedMapLong = strings.map(function(string) {
+  return string.toUpperCase()
+})
+console.log(stringsUpcasedMapLong)
 
 // #  3. Start with an array of hashes and create a new array of string values from each hash's :name key.
 // #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes ["Alice", "Blane"]. Use .forEach and .map.
 
-// ..forEach
+// .forEach
+
+let peopleInfo = [{name: "Alice", age: 27}, {name: "Blane", age: 16}]
+let peoplesName = []
+
+peopleInfo.forEach(function(person) {
+  peoplesName.push(person.name)
+})
+console.log(peoplesName)
+
+let peoplesNames = []
+
+peopleInfo.forEach(person => peoplesNames.push(person.name))
+console.log(peoplesNames)
 
 
 // .map
 
+let personsName = peopleInfo.map(person => person.name)
+console.log(personsName)
+
+let personNameLong = peopleInfo.map(function(person) {
+  return person.name
+})
+console.log(personNameLong)
+
 // #  4. Start with an array of numbers and create a new array with each number plus 7.
 // #     For example, [1, 2, 3] becomes [8, 9, 10]. Use .forEach and .map.
 
-// ..forEach
+// .forEach
 
 
 // .map
