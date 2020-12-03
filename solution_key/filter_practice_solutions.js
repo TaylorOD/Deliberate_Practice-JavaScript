693569e2c0acd5e53708acda5416e8357e14a1f4
Taylor Dorsett <taylorsunwantedemails@gmail.com>
2020-12-03 12:55:39 -0600

    Update filter_practice_solutions.js
    - add first two solutions

--- solution_key/filter_practice_solutions.js
+++ solution_key/filter_practice_solutions.js
@@ -1,8 +1,28 @@
 // #  1. Start with an array of numbers and create a new array with only the numbers less than 20.
 // #     For example, [2, 32, 80, 18, 12, 3] becomes [2, 18, 12, 3].
 
+let numbers = [2, 32, 80, 18, 12, 3] 
+
+const lessThanTwenty = numbers.filter(number => number < 20)
+console.log(lessThanTwenty)
+
+const lessThanTwentyLong = numbers.filter(function (number) {
+  return number < 20
+})
+console.log(lessThanTwentyLong)
+
 // #  2. Start with an array of strings and create a new array with only the strings that start with the letter "w".
 // #     For example, ["winner", "winner", "chicken", "dinner"] becomes ["winner", "winner"].
+
+let strings = ["winner", "winner", "chicken", "dinner"]
+
+const winningStrings = strings.filter(string => string[0] === "w")
+console.log(winningStrings)
+
+const winningStringsLong = strings.filter(function (string) {
+  return string[0] === "w"
+})
+console.log(winningStringsLong)
 
 // #  3. Start with an array of hashes and create a new array with only the hashes with prices greater than 5 (from the :price key).
 // #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}].
