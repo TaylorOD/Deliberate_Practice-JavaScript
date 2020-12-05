bea4ddf82aad826e73a47696b3f37c71d3a54afe
Taylor Dorsett <taylorsunwantedemails@gmail.com>
2020-12-05 13:37:33 -0600

    Create reduce_practice_solutions.js
    - add 10 questions

---
+++ solution_key/reduce_practice_solutions.js
@@ -0,0 +1,29 @@
+// #  1. Start with an array of numbers and compute the sum of all the numbers.
+// #     For example, [5, 10, 8, 3] becomes 26.
+
+// #  2. Start with an array of strings and combine them all into a single string.
+// #     For example, ["volleyball", "basketball", "badminton"] becomes "volleyballbasketballbadminton".
+
+// #  3. Start with an array of hashes and compute the sum of the prices (from the :price key).
+// #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes 105.
+
+// #  4. Start with an array of numbers and compute the the minumum number.
+// #     For example, [5, 10, 8, 3, 9] becomes 3.
+
+// #  5. Start with an array of strings and compute the total length of all the strings.
+// #     For example, ["volleyball", "basketball", "badminton"] becomes 29.
+
+// #  6. Start with an array of hashes and find the hash with the lowest price (from the :price key).
+// #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "pencil", price: 1}.
+
+// #  7. Start with an array of numbers and compute product of all the numbers.
+// #     For example, [5, 10, 8, 3] becomes 1200.
+
+// #  8. Start with an array of strings and combine them all into a single string, separated by dashes.
+// #     For example, ["volleyball", "basketball", "badminton"] becomes "-volleyball-basketball-badminton-".
+
+// #  9. Start with an array of hashes and find the hash with the shortest name (from the :name key).
+// #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "book", price: 4}.
+
+// # 10. Start with an array of numbers and compute the maximum number.
+// #     For example, [5, 10, 8, 3] becomes 10.
