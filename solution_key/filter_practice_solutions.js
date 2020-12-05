a1d89a21516ae7b171a03976f540c65efbdb1396
Taylor Dorsett <taylorsunwantedemails@gmail.com>
2020-12-05 13:30:19 -0600

    Update filter_practice_solutions.js
    - add 9th solution

--- solution_key/filter_practice_solutions.js
+++ solution_key/filter_practice_solutions.js
@@ -105,5 +105,15 @@ console.log(stringsOtherThanBLong)
 // #  9. Start with an array of hashes and create a new array with only the hashes with prices less than 10 (from the :price key).
 // #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "pencil", price: 1}, {name: "book", price: 4}].
 
+let itemsInfo = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
+
+const itemsLessThanTen = itemsInfo.filter(item => item.price < 10)
+console.log(itemsLessThanTen)
+
+const itemsLessThanTenLong = itemsInfo.filter(function (item) {
+  return item.price < 10
+})
+console.log(itemsLessThanTenLong)
+
 // # 10. Start with an array of numbers and create a new array with only the odd numbers.
 // #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [5, 1, 9, 7].
